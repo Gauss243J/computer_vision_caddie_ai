5ece977f95e5310a6abeeab5c9914c15e8be0b4b
Gauss243J <78795548+Gauss243J@users.noreply.github.com>
2024-09-15 16:48:29 +0200

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -11,8 +11,8 @@ router.get('/', async (req, res) => {
 // Afficher tous les produits
 router.get('/products', async (req, res) => {
     try {
-        const products = await Produict.find(); // Correction du nom du modèle
-        res.json(produicts); // Retourne les produits en format JSON
+        const products = await Product.find(); // Correction du nom du modèle
+        res.json(products); // Retourne les produits en format JSON
     } catch (error) {
         res.status(500).json({ message: 'Erreur lors de la récupération des produits', error });
     }
