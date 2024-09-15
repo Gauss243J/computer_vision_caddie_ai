8b7685061385105d8a6540e2abc3722db8b18f29
Gauss243J <78795548+Gauss243J@users.noreply.github.com>
2024-09-15 16:32:27 +0200

    Update produicts.js
    add

--- routes/produicts.js
+++ routes/produicts.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/Produict');
 
+// Afficher tous les produits
+router.get('/', async (req, res) => {
+    try {
+        const products = await Produict.find(); // Correction du nom du modèle
+        res.json(produicts); // Retourne les produits en format JSON
+    } catch (error) {
+        res.status(500).json({ message: 'Erreur lors de la récupération des produits', error });
+    }
+});
+
+module.exports = router;
